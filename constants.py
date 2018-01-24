3b0565633f404fc87101c81aafc3b70df7fc594b
N4th4lie <N4th4lie@users.noreply.github.com>
2018-01-24 13:02:29 +1100

    Ajout Create_labyrinth.py et code en anglais

---
+++ constants.py
@@ -0,0 +1,26 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 11:00:00 2018
+
+@author: N4th4lie
+"""
+
+""" CONSTANTS: filenames, window parameters,..."""
+
+# Window parameters
+side_nb_sprites = 15
+sprite_size = 30
+side_size = side_nb_sprites * sprite_size
+
+# Window personalization
+window_title = "Aide MAC GYVER à s'échapper !"
+image_icone = "images\lisasimpson.ico"
+
+# File Labyrinth.txt
+fic_labyrinth = "labyrinth.txt"
+
+# Images
+image_ground = "images\ground.jpg"
+image_wall = "images\wallbrick.png"
+image_finish = "images\door_open.png"
+
--- macgyver.py
+++ macgyver.py
@@ -5,77 +5,19 @@ Created on Tue Jan 23 15:44:00 2018
 @author: N4th4lie
 """
 
-# Jeu (interface graphique,...)
 import pygame
 
-# pour les QUIT, KEYDOWN,...
+# QUIT, KEYDOWN,...
 from pygame.locals import *
 
 pygame.init()
 
-""" CONSTANTES DU JEU """
+""" CONSTANTS: filenames, parameters,..."""
+from constants import *
 
-#Paramètres de la fenêtre
-nombre_sprite_cote = 15
-taille_sprite = 30
-cote_fenetre = nombre_sprite_cote * taille_sprite
-
-#Personnalisation de la fenêtre
-titre_fenetre = "Aide MAC GYVER à s'échapper !"
-image_icone = "images\lisasimpson.ico"
-
-#Fichier Labyrinth.txt
-fic_labyrinth = "labyrinth.txt"
-
-#Listes des images du jeu
-image_fond = "images\ground.jpg"
-image_mur = "images\wallbrick.png"
-image_arrivee = "images\door_open.png"
-
-""" Fin Constantes du jeu """
-
-fenetre = pygame.display.set_mode((cote_fenetre, cote_fenetre))
-pygame.display.set_caption(titre_fenetre)
-imico = pygame.image.load(image_icone)
-pygame.display.set_icon(imico)
-
-# !!!!!!!!! la méthode convert() plante le programme
-# fond = pygame.image.load(image_fond).convert()
-fond = pygame.image.load(image_fond)
-fenetre.blit(fond, (0,0))
-
-"""
-AFFICHAGE DU LABYRINTHE à partir d'un fichier texte
-"""	
-
-# Ouverture du fichier texte et parcours des lignes avec WITH OPEN()
-# Dans le fichier texte : M=mur, D=départ, A=arrivée, x=rien
-with open(fic_labyrinth, "r", encoding="utf8") as f:
-    labyrinth = []
-    for line in f:
-        line_level = [sprite for sprite in line if sprite!="\n"]
-        labyrinth.append(line_level)
-            
-# Chargement des images :
-mur = pygame.image.load(image_mur)
-arrivee = pygame.image.load(image_arrivee)
-# l'arrivée deoit contenir de la transparence mais problème convert()
-# arrivee = pygame.image.load(image_arrivee).convert_alpha()
-
-# Parcours des lignes du labyrinthe
-for num_line,line in enumerate(labyrinth):
-    # Parcours des sprites des lignes
-	for num_sprite,sprite in enumerate(line):
-	    #On calcule la position réelle en pixels
-		x = num_sprite * taille_sprite
-		y = num_line * taille_sprite
-		if sprite == 'M':		   
-		    fenetre.blit(mur, (x,y))
-		elif sprite == 'A':		
-		    fenetre.blit(arrivee, (x,y))
-"""
-Fin Affichage du labyrinthe 
-"""	                
+""" CREATE_LABYRINTH creates the labyrinth screen """
+from create_labyrinth import *
+create_labyrinth()
 
 # Rafraîchissement écran                
 pygame.display.flip()
