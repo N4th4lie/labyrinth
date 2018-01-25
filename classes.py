50690745799cfbbf7a7bbe2f28e1e003941560c5
N4th4lie <N4th4lie@users.noreply.github.com>
2018-01-25 14:09:50 +1100

    Etape 2 avec classes Labwindow et Character. Images png 30x30

---
+++ classes.py
@@ -0,0 +1,80 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 2018
+
+@author: N4th4lie
+"""
+
+import pygame
+# QUIT, KEYDOWN,...
+from pygame.locals import *
+
+""" CONSTANTS: filenames, window parameters,..."""
+from constants import *
+
+
+""" Labyrinth windows """
+class Labwindow:
+
+    def __init__(self, window, structure):
+        self.window = window
+        self.structure = structure
+       
+    def display(self):  
+        
+        # load images:
+        ground = pygame.image.load(image_ground)
+        wall = pygame.image.load(image_wall)
+        finish = pygame.image.load(image_finish)
+        # The finish line must contain transparence but problem occurs with convert()
+        # finish = pygame.image.load(image_finish).convert_alpha()
+    
+        # Initialization
+        self.window.blit(ground, (0,0))
+        
+        # Run through the Labyrinth lines
+        for num_line,line in enumerate(self.structure):
+            # Run through sprites of each line
+            for num_sprite,sprite in enumerate(line):
+                # calculate the real position in pixels
+                x = num_sprite * sprite_size
+                y = num_line * sprite_size
+                if sprite == 'W':
+                    self.window.blit(wall, (x,y))
+                elif sprite == 'F':
+                    self.window.blit(finish, (x,y))
+
+""" Characters of the game """
+class Character:
+
+	def __init__(self, image, case_x, case_y):
+		self.image = pygame.image.load(image)
+		# Position in 15x15 cases (utilisation easier than pixels) 
+       # and in pixels (parameters of blit())
+		self.case_x = case_x
+		self.case_y = case_y
+		self.x = case_x * sprite_size
+		self.y = case_y * sprite_size
+	
+	def move(self, direction,structure):
+		""" characters can move """
+		# Right direction and not at the end of the window and not a wall on the right
+		if direction == '275' and self.case_x != (side_nb_sprites - 1) and structure[self.case_y][self.case_x+1] != 'W':
+		    self.case_x += 1
+		
+		# Left direction and not at the beginning of the window and not a wall on the left
+		if direction == '276' and self.case_x != 0 and structure[self.case_y][self.case_x-1] != 'W':
+			self.case_x -= 1
+		
+		# Up direction and not at the top of the window and not a wall above
+		if direction == '273' and self.case_y != 0 and structure[self.case_y-1][self.case_x] != 'W':
+			self.case_y -= 1
+		
+		# Down direction and not at the bottom of the window and not a wall below
+		if direction == '274' and self.case_y != (side_nb_sprites - 1) and structure[self.case_y+1][self.case_x] != 'W':
+			self.case_y += 1
+                
+       # Calculate new position in pixels
+		self.x = self.case_x * sprite_size
+		self.y = self.case_y * sprite_size
+         
--- macgyver.py
+++ macgyver.py
@@ -15,17 +15,91 @@ pygame.init()
 """ CONSTANTS: filenames, parameters,..."""
 from constants import *
 
-""" CREATE_LABYRINTH creates the labyrinth screen """
-from create_labyrinth import *
-create_labyrinth()
+""" CLASSES : Labwindow et Characters """
+from classes import *
 
-# Rafraîchissement écran                
+# Open a window
+window = pygame.display.set_mode((side_size,side_size))
+pygame.display.set_caption(window_title)
+icone = pygame.image.load(image_icone)
+pygame.display.set_icon(icone)
+
+# Load the labyrinth structure from a text file.
+# The labyrinth_structure gives positions of walls, start and finish cases.
+# Read the text file in a WITH OPEN() and write the result in a list of lists
+# W=wall, S=start, F=finish, n=nothing
+with open(fic_labyrinth, "r", encoding="utf8") as f:
+    labyrinth_structure = []
+    for line in f:
+        line_level = [sprite for sprite in line if sprite!="\n"]
+        labyrinth_structure.append(line_level)
+
+# Create the labyrinth window
+labyrinth = Labwindow(window, labyrinth_structure)
+# Display
+labyrinth.display()
+
+# Refresh the window               
+pygame.display.flip()
+
+""" Etape 2 du projet : créer Mac Gyver et le diriger vers la sortie  """
+
+# Start and Finish localisation :
+for num_line, line in enumerate(labyrinth_structure):
+    if 'S' in line:
+        start_case_x = line.index('S')
+        start_case_y = num_line
+    if 'F' in line:
+        finish_case_x = line.index('F')
+        finish_case_y = num_line
+    
+# Create the caracter Mac Gyver
+macgyver = Character(macgyver_im, start_case_x, start_case_y)
+# Affichage de Mac Gyver
+window.blit(macgyver.image, (macgyver.x, macgyver.y))
+
+# Refresh the window               
 pygame.display.flip()
 
 continuer = True
+succeed = False
 while continuer:
+    #Limitation speed for while
+    pygame.time.Clock().tick(30)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = False
+            
+        elif event.type == KEYDOWN:
+		     
+            # Key Tab for moving Mac Gyver
+            if (not succeed) and event.key in (K_RIGHT, K_LEFT, K_UP, K_DOWN):
+                macgyver.move(str(event.key),labyrinth_structure)
+ 		     
+            # Key Tab for moving Mac Gyver
+            if succeed :
+               continuer = False
+   
+    # Display the initial labyrinth without Mac Gyver
+    labyrinth.display()
+
+    # Refresh the window with Mac Gyver's new position
+    window.blit(macgyver.image, (macgyver.x, macgyver.y))
+    pygame.display.flip()
+    
+    if macgyver.case_x == finish_case_x and macgyver.case_y == finish_case_y:
+        
+        # Chargement d'une font:
+        #calibri_font = pygame.font.SysFont("Calibri", 20)
+        arial_font = pygame.font.SysFont("Arial", 20)
+        # Font reçoit un fichier
+        # Création d'une surface pour le score lui-même:
+        succeed = True
+        end_message1 = arial_font.render("FELICITATIONS ! Mac Gyver est libre !", True, (223, 255, 0))
+        end_message2 = arial_font.render("Appuis sur une touche pour quitter. Au revoir.", True, (223, 255, 0))
+        window.blit(end_message1, (0, 5))
+        window.blit(end_message2, (0, 20))
+        pygame.display.flip()
 
 pygame.quit()
