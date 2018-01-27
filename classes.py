de04b934b0f8959ca3f9b547db1a1a6af70252c9
N4th4lie <N4th4lie@users.noreply.github.com>
2018-01-27 12:00:29 +1100

    Etape 4 : Changer la fin du jeu

--- classes.py
+++ classes.py
@@ -28,9 +28,10 @@ class Labwindow:
         ground = pygame.image.load(image_ground)
         wall = pygame.image.load(image_wall)
         finish = pygame.image.load(image_finish)
-        #guard = pygame.image.load(guard_im)
-        # The finish line must contain transparence but problem occurs with convert()
+        # The finish line must contain transparence but problem occurs 
+        # with convert().
         # finish = pygame.image.load(image_finish).convert_alpha()
+        guard = pygame.image.load(guard_im)
     
         # Initialization
         self.window.blit(ground, (0,0))
@@ -46,14 +47,18 @@ class Labwindow:
                     self.window.blit(wall, (x,y))
                 elif sprite == 'F':
                     self.window.blit(finish, (x,y))
+                    self.window.blit(guard, (x,y))
 
         # If the items are ready, run through the items list.
         for item in self.items:
             self.window.blit(item.image, (item.x,item.y))
         # Items counter :
         arial_font = pygame.font.SysFont("Arial", 20)
-        items_counter = arial_font.render("Nombre d'objets collectés : "+str(3-len(self.items)), True, (223, 255, 0))
-        self.window.blit(items_counter, (10, 14*30))
+        n_items = str(3-len(self.items))
+        items_counter = arial_font.render("Nombre d'objets collectés : " + \
+                                          n_items + "/3", True, (223, 255, 0))
+        self.window.blit(items_counter, ((side_nb_sprites-1)/2*sprite_size, \
+                                         (side_nb_sprites-1)*sprite_size))
    
 """ Characters of the game """
 class Character:
@@ -69,20 +74,25 @@ class Character:
 	
 	def move(self, direction,structure):
 		""" characters can move """
-		# Right direction and not at the end of the window and not a wall on the right
-		if direction == '275' and self.case_x != (side_nb_sprites - 1) and structure[self.case_y][self.case_x+1] != 'W':
+		# Right direction and not at the end of the window,not a wall on the right
+		if direction == '275' and self.case_x != (side_nb_sprites - 1) and \
+          structure[self.case_y][self.case_x+1] != 'W':
 		    self.case_x += 1
 		
-		# Left direction and not at the beginning of the window and not a wall on the left
-		if direction == '276' and self.case_x != 0 and structure[self.case_y][self.case_x-1] != 'W':
+		# Left direction and not at the beginning of the window 
+       # and not a wall on the left
+		if direction == '276' and self.case_x != 0 and \
+          structure[self.case_y][self.case_x-1] != 'W':
 			self.case_x -= 1
 		
 		# Up direction and not at the top of the window and not a wall above
-		if direction == '273' and self.case_y != 0 and structure[self.case_y-1][self.case_x] != 'W':
+		if direction == '273' and self.case_y != 0 and \
+          structure[self.case_y-1][self.case_x] != 'W':
 			self.case_y -= 1
 		
 		# Down direction and not at the bottom of the window and not a wall below
-		if direction == '274' and self.case_y != (side_nb_sprites - 1) and structure[self.case_y+1][self.case_x] != 'W':
+		if direction == '274' and self.case_y != (side_nb_sprites - 1) and \
+          structure[self.case_y+1][self.case_x] != 'W':
 			self.case_y += 1
                 
        # Calculate new position in pixels
--- macgyver.py
+++ macgyver.py
@@ -44,12 +44,10 @@ labyrinth.display()
 # Refresh the window               
 pygame.display.flip()
 
-""" Etape 3 du projet : créer les objets et gérer leur collecte  """
-
-# Create the characters and the items
+# To create the characters and the items, you need to know their position
 
 # Start (start_case_x,start_case_y) is Mac Gyver's initial position
-# Finish (finish_case_x,finish_case_y) : for the open door and the guard
+# Finish (finish_case_x,finish_case_y) : the guard's place
 # Free : the list of all the free cases for the items
 free = []
 for num_line, line in enumerate(labyrinth_structure):
@@ -83,18 +81,18 @@ labyrinth.display()
 # Refresh the window               
 pygame.display.flip()
 
-continuer = True
-succeed = False
-while continuer:
+go_on = True
+game_over = False
+while go_on:
     #Limitation speed for while
     pygame.time.Clock().tick(30)
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            continuer = False
+            go_on = False
         elif event.type == KEYDOWN:
             # Key Tab for moving Mac Gyver
-            if (not succeed) and event.key in (K_RIGHT, K_LEFT, K_UP, K_DOWN):
+            if (not game_over) and event.key in (K_RIGHT, K_LEFT, K_UP, K_DOWN):
                 macgyver.move(str(event.key),labyrinth_structure)
  
                # if Mac Gyver is reaching an item, 
@@ -102,26 +100,36 @@ while continuer:
                 for i,item in enumerate(labyrinth.items):
                     if macgyver.x == item.x and macgyver.y == item.y:
                         del labyrinth.items[i]
-                        break
-            
-            # Quit game after you won and confirmed
-            if succeed :
-               continuer = False
-   
+                        break    
+            # Quit game after the game over message has been read
+            if game_over :
+               go_on = False
+       
     # Display the labyrinth with items
     labyrinth.display()
-
     # Refresh the window with Mac Gyver's new position
     window.blit(macgyver.image, (macgyver.x, macgyver.y))
-    
     pygame.display.flip()
     
-    if macgyver.case_x == finish_case_x and macgyver.case_y == finish_case_y:
-        # surface for message :
-        succeed = True
+    # If Mac Gyver reached the finish:
+    if macgyver.case_x == finish_case_x \
+       and macgyver.case_y == finish_case_y:
+          
+        game_over = True
         arial_font = pygame.font.SysFont("Arial", 20)
-        end_message1 = arial_font.render("FELICITATIONS ! Mac Gyver est libre !", True, (223, 255, 0))
-        end_message2 = arial_font.render("Appuis sur une touche pour quitter. Au revoir.", True, (223, 255, 0))
+        end_message2=arial_font.render("Appuis sur une touche pour quitter."\
+                                       +" Au revoir.", True, (223, 255, 0))
+        # Mac Gyver reached the finish case.
+        # If no more items displayed on the window, you won or lost:
+        if labyrinth.items==[]:
+            end_message1=arial_font.render("FELICITATIONS !" \
+                                           + "Mac Gyver est libre !",\
+                                      True, (223, 255, 0))
+        else:
+            end_message1=arial_font.render("PERDU !" \
+                                           + "Le gardien arrête Mac Gyver !",\
+                                      True, (223, 255, 0))
+        
         window.blit(end_message1, (10, 5))
         window.blit(end_message2, (10, 20))
         pygame.display.flip()
